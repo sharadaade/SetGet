6038e606543af8116d9ffb16c9dfd3ff08834ff6
sharadaade <aadesharad@gmail.com>
2024-05-31 14:36:25 +0530

    Add project files.

---
+++ SetGet/BankAccount.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetGet
+{
+    class Account
+    {
+        private string name;
+        private int acbal;
+        
+        public string Name
+        {
+            set
+            {
+                name = value;
+            }
+
+            get
+            {
+                return name;
+            }
+        }
+
+        public int AcBal
+        {
+
+            set
+            {
+                if(value >= 0)
+                {
+                    acbal = value;
+                }
+                else
+                {
+                    Console.WriteLine("Enter valid amount");
+                }
+            }
+
+            get
+            {
+                return acbal;
+            }
+        }
+
+        public int FormattedBalance
+        {
+            get
+            {
+                return acbal;
+            }
+        }
+    }
+
+    internal class BankAccount
+    {
+        static void Main(string[] args)
+        {
+            Account a = new Account();
+
+            a.Name = "Sharad";
+            a.AcBal = 50000;
+            int b = a.FormattedBalance;
+            Console.WriteLine("Name : {0}",a.Name);
+            Console.WriteLine("Balance : {0}",a.AcBal);
+            Console.WriteLine("Banance from FormattedBalance {0}",b);
+        }
+    }
+}
---
+++ SetGet/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace SetGet
+{
+    class Rectangle
+    {
+        private int width;
+        private int height;
+
+
+
+        public int Width
+        {
+            set 
+            {
+                if (value > 0)
+                {
+                    width = value;
+                }
+                else
+                {
+                    Console.WriteLine("Enter +ve value");
+                }
+            }
+            get
+            {
+                return width;
+            }
+        }
+
+        public int Height
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    height = value;
+                }
+                else
+                {
+                    Console.WriteLine("Enter +ve value");
+                }
+            }
+            get
+            {
+                return height;
+            }
+        }
+
+        public int RectArea
+        {
+            get
+            {
+                return width * height;
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Rectangle();
+            r1.Width = 10;
+            r1.Height = 10;
+
+            int result = r1.Width * r1.Height;
+
+            Console.WriteLine("Area is {0}",result);
+        }
+    }
+}
